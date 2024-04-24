3c6d1767eccb330db1692723bd0ec110468f4d72
Kaltrinë Baliu <kaltrine.baliu@student.uni-pr.edu>
2024-04-24 21:51:08 +0200

    Local storage

--- kanban_borad/src/components/Kanban/Kanban.jsx
+++ kanban_borad/src/components/Kanban/Kanban.jsx
@@ -1,13 +1,12 @@
-import { useRef, useState } from 'react';
+import { useRef, useState, useEffect } from 'react';
 import './Kanban.css';
 import Column from '../Column/Column';
-import { StatusEnum } from '../../types/index'; 
-
+import { StatusEnum } from '../../types/index';
+import useLS from './UseLS'; // Assuming UseLS.jsx is in the same directory as Kanban.jsx
 
 const Kanban = ({ data }) => {
-  const [dataLists, setDataLists] = useState(data);
+  const [dataLists, setDataLists] = useLS('kanbanData', data); // Initialize data from local storage if available
   const [dragging, setDragging] = useState(false);
-  
 
   const dragItem = useRef(null);
   const dragNode = useRef(null);
@@ -26,17 +25,17 @@ const Kanban = ({ data }) => {
   const handleDragEnter = (event, task) => {
     const currentItem = dragItem.current;
     const draggedTask = dataLists[currentItem.tableIndex].tasks[currentItem.taskIndex];
-    
+
     if (event.target !== dragNode.current) {
       const targetRect = event.target.getBoundingClientRect();
       const offsetY = event.clientY - targetRect.top;
       const isNearBottom = offsetY > targetRect.height / 2; // Check if drag is near bottom of the column
-      
+
       if (isNearBottom) {
         if (dataLists[task.tableIndex].status === StatusEnum.FINAL && draggedTask.id % 2 === 0) {
-          return; 
+          return;
         }
-  
+
         setDataLists((oldList) => {
           const newList = JSON.parse(JSON.stringify(oldList));
           const removedTask = newList[currentItem.tableIndex].tasks.splice(currentItem.taskIndex, 1)[0];
@@ -56,7 +55,6 @@ const Kanban = ({ data }) => {
       }
     }
   };
-  
 
   const handleDragEnd = () => {
     setDragging(false);
@@ -75,12 +73,17 @@ const Kanban = ({ data }) => {
     return 'single-task';
   };
 
+  // Update local storage when dataLists changes
+  useEffect(() => {
+    localStorage.setItem('kanbanData', JSON.stringify(dataLists));
+  }, [dataLists]);
+
   return (
     <div className='board-container'>
       <div className="container">
         <div className="board-content">
           {dataLists.map((table, tableIndex) => (
-            <Column 
+            <Column
               key={table.id}
               status={table.status}
               tasks={table.tasks}
@@ -94,7 +97,7 @@ const Kanban = ({ data }) => {
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default Kanban;
